c96c1c4a8d97aee384f000e96a7b00303da0e11d
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-24 16:57:39 +0100

    sistemato script controllo tutti elementi array

--- mail/js/scriptmail.js
+++ mail/js/scriptmail.js
@@ -7,16 +7,21 @@ const button = document.getElementById('check');
 button.addEventListener("click", function () {
     const email = document.getElementById('email').value;
     document.getElementById('email').value = '';
+    let find = false;
 
     for (let i = 0; i < emailList.length; i++) {
+        const element = emailList[i];
+        if (element.toLowerCase() == email.toLowerCase()){
+            find = true;
+        }
+    }
         // controlla che sia nella lista di chi può accedere,
         // stampa un messaggio appropriato sull’esito del controllo.
-        if (email == emailList[i]) {
+        if (find == true) {
             container.append(div);
             div.innerHTML = "la tua email è nella lista, puoi proseguire";
         } else {
             container.append(div);
             div.innerHTML = "la tua email non è nella lista, riprova";
         }
-    }
 })
